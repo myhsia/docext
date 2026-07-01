c977a01f2428d85824ab107568028afcf85f5545
Mingyu Hsia <myhsia@outlook.com>
2026-07-01 14:51:41 +0800

    Version 0.2C released.

--- announcement.md
+++
@@ -1,3 +0,0 @@
-## Version 0.2B released
-
-- New feature: Compressing overlong keys in the `keyval` environment for the `\keys` macro.
--- build.lua
+++ build.lua
@@ -6,8 +6,8 @@
     Basic Information: Do Check Before Push
 --]==========================================================================]--
 module              = "docext"
-version             = "v0.2B"
-date                = "2026-06-27"
+version             = "v0.2C"
+date                = "2026-07-01"
 maintainer          = "Mingyu Xia"
 uploader            = "Mingyu Xia"
 maintainid          = "myhsia"
